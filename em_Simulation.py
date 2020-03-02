6773955716de7288fd0369bb68a4951c78f652f8
Linda Winkler <winklerl@mnstate.edu>
2020-03-02 16:55:33 -0600

    Add files via upload

--- em_Simulation.py
+++ em_Simulation.py
@@ -15,8 +15,8 @@ def HH_field(N, I, L):
 
 
     """
-    mu = 4 * np.pi
-    return 8 * mu * N * I / (125 * L)
+    mu = 4 * (1.0e-7)*np.pi # Corrected mu0
+    return 8 * mu * N * I / (np.sqrt(125 )* L)
 
 ####################################################
 def Lorentz_force(q, v, B):
@@ -36,51 +36,4 @@ def Lorentz_force(q, v, B):
 
 # the voltage difference experience by the electron
 
-V_i = -60
-V_f = -135
-dV = V_f - V_i
 
-q = -1.6e-19
-m = 9.1e-31
-
-# the electron speed assuming that it is at rest to begin with
-e_speed = np.sqrt(2 * q * dV / m)
-print(e_speed)
-c = 2.99e8
-print(e_speed/c)
-
-# gives the magnitude but not the direction of the B field
-Bmag = HH_field(300, 4, 0.2)
-# setting the coordinates to align with what's in my notebook
-B_field = np.array([Bmag, 0, 0])
-
-# the lists below will contain the history of the position components
-e_x = [0]
-e_y = [0]
-e_z = [0]
-
-e_vel = np.array([0, 0, e_speed])
-e_mom = m * e_vel
-e_pos = np.array([0, 0, 0])
-
-t = 0
-dt = 1e-17
-
-while e_pos[2] >= 0:
-    
-    e_force = Lorentz_force(q, e_vel, B_field)
-    e_mom = e_mom + e_force * dt
-    e_vel = e_mom / m
-    e_pos = e_pos + e_vel * dt
-    
-    # below are 
-    e_x.append(e_pos[0])
-    e_y.append(e_pos[1])
-    e_z.append(e_pos[2])
-    
-    t += dt
-
-# below is the history of the position components as arrays
-x_array = np.array(e_x)
-y_array = np.array(e_y)
-z_array = np.array(e_z)
